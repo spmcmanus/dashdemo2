a297d0fb6963651b7d4567fde0de5b6ba4a92ab0
Shean McManus <smcmanus@psclistens.com>
2017-09-21 10:56:00 -0500

    updates

--- src/webparts/dashdemo/components/Dashdemo.tsx
+++ src/webparts/dashdemo/components/Dashdemo.tsx
@@ -3,6 +3,11 @@ import * as jquery from 'jquery';
 
 import { IDashdemoProps } from './IDashdemoProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import {
+  SearchBox
+} from 'office-ui-fabric-react/lib/SearchBox';
+
+//import './SearchBox.Small.Example.scss';
 
 import Iframe from 'react-iframe';
 
@@ -33,6 +38,14 @@ export interface linksState {
       "linkDesc": string;
     }
   ];
+  allLinks: [
+    {
+      "Title": string;
+      "AuthorId": string;
+      "linkURL": string;
+      "linkDesc": string;
+    }
+  ];
   linkSelectedURL: string;
   rowClasses: string;
   embedClasses: string;
@@ -46,9 +59,15 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
 
     console.log('document card dtype', DocumentCardType.compact)
 
-
     this.state = {
       links:
+      [{
+        "Title": '',
+        "AuthorId": '',
+        "linkURL": '',
+        "linkDesc": '',
+      }],
+      allLinks:
       [{
         "Title": '',
         "AuthorId": '',
@@ -63,6 +82,40 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
     this.onCardClick = this.onCardClick.bind(this);
   }
 
+  public searchOnChange(searchValue) {
+    if (searchValue == '') {
+      this.componentDidMount();
+    }
+  }
+  public search(searchValue) {
+    
+    var filteredLinks = this.state.links;
+
+    for (var x=0;x<filteredLinks.length;x++) {
+      var temp = 0;
+      console.log(filteredLinks[x]["documentAuthor"])
+      if (filteredLinks[x]["Title"].toLowerCase().indexOf(searchValue.toLowerCase()) >= 0) {
+        temp = 1;
+      } else if (filteredLinks[x]["documentAuthor"].toLowerCase().indexOf(searchValue.toLowerCase()) >= 0) {
+        temp = 1;
+      }
+
+      if (temp == 0) {
+        filteredLinks[x] = null;
+      }
+
+    }
+    this.setState({
+      links: filteredLinks,
+      allLinks: this.state.allLinks,
+      linkSelectedURL: '',
+      rowClasses: this.state.rowClasses,
+      embedClasses: this.state.embedClasses,
+      cardType: DocumentCardType.normal
+    });
+
+  }
+
   // card click listener
   public onCardClick(link, e) {
     console.log("clicky")
@@ -71,7 +124,7 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
       console.log("display link instead");
       var win = window.open(link.linkURL, '_blank');
       win.focus();
-      //
+
     } else {
       console.log("work off attachment")
       const linkId = link.ID;
@@ -86,6 +139,7 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
         console.log("attachmentFullURL", attachmentFullURL);
         this.setState({
           links: this.state.links,
+          allLinks: this.state.allLinks,
           linkSelectedURL: attachmentFullURL,
           rowClasses: 'ms-Grid-col ms-sm4',
           embedClasses: "",
@@ -105,6 +159,7 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
     if (this.state.cardType == DocumentCardType.normal) {
       this.setState({
         links: this.state.links,
+        allLinks: this.state.allLinks,
         linkSelectedURL: this.state.linkSelectedURL,
         rowClasses: this.state.rowClasses,
         embedClasses: this.state.embedClasses,
@@ -113,6 +168,7 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
     } else {
       this.setState({
         links: this.state.links,
+        allLinks: this.state.allLinks,
         linkSelectedURL: this.state.linkSelectedURL,
         rowClasses: this.state.rowClasses,
         embedClasses: this.state.embedClasses,
@@ -121,9 +177,55 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
     }
   }
 
+  public trim_array(test_array) {
+    var index = -1,
+      arr_length = test_array ? test_array.length : 0,
+      resIndex = -1,
+      result = [];
+
+    while (++index < arr_length) {
+      var value = test_array[index];
+
+      if (value) {
+        result[++resIndex] = value;
+      }
+    }
+    return result;
+  }
+
+/*
+  public filter(ev: React.FormEvent<HTMLElement>, isChecked: boolean) {
+    console.log(ev);
+    console.log('filter state', this.state);
+    console.log(`The option has been changed to ${isChecked}.`);
+
+    var filteredLinks = this.state.links;
+    filteredLinks.map((link, key) => {
+      if (link["AttachmentFiles"].results.length > 0) {
+        var fileExt = link["AttachmentFiles"].results[0].FileName.split(".")[1].trim();
+        if (fileExt != 'docx' && fileExt != 'doc') {
+          filteredLinks[key] = null;
+        }
+      } else {
+        filteredLinks[key] = null;
+      }
+    });
+
+
+    this.setState({
+      links: filteredLinks,
+      allLinks: this.state.allLinks,
+      linkSelectedURL: '',
+      rowClasses: this.state.rowClasses,
+      embedClasses: this.state.embedClasses,
+      cardType: DocumentCardType.normal
+    });
+  }
+*/
   public clearSelected() {
     this.setState({
       links: this.state.links,
+      allLinks: this.state.allLinks,
       linkSelectedURL: '',
       rowClasses: this.state.rowClasses,
       embedClasses: this.state.embedClasses,
@@ -149,6 +251,7 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
         console.log(resultData)
         reactHandler.setState({
           links: resultData.d.results,
+          allLinks: resultData.d.results,
           linkSelectedURL: '',
           rowClasses: 'ms-Grid-col ms-sm12',
           embedClasses: '',
@@ -165,7 +268,9 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
 
   public render(): React.ReactElement<IDashdemoProps> {
     console.log("state", this.state)
-    if (this.state.links[0].Title == '') {
+    var showLinks = this.state.links.filter(function(n){ return n != null })
+    console.log("show links",showLinks)
+    if (showLinks[0].Title == '') {
       return (
         <div>Loading...</div>
       );
@@ -212,12 +317,17 @@ export default class Dashdemo extends React.Component<IDashdemoProps, linksState
     } else {
       const theseIncidents = this.state.links;
       return (
-        <div>
-          <div className={styles.buttonRight}>
-            <PrimaryButton
-              text='Toggle Card Display'
-              onClick={() => this.toggleCardDisplay()}
-            />
+
+        <div className="ms-Grid">
+          <div className="ms-Grid-row">
+            <div className="ms-Grid-col ms-sm4">
+              <div className='ms-SearchBoxSmallExample'>
+                <SearchBox
+                  onChange={(newValue) => this.searchOnChange(newValue)}
+                  onSearch={(newValue) => this.search(newValue)}
+                />
+              </div>
+            </div>
           </div>
           <LinkListMarkup
             links={this.state.links}
--- src/webparts/dashdemo/components/linkListMarkup.tsx
+++ src/webparts/dashdemo/components/linkListMarkup.tsx
@@ -7,168 +7,228 @@ import styles from '../resources/Dashdemo.module.scss';
 
 // Office-Ui Fabric Components
 import {
-	DocumentCard,
-	DocumentCardTitle,
-	DocumentCardActivity,
-	DocumentCardPreview,
-	DocumentCardActions,
-	IDocumentCardPreviewProps,
-	DocumentCardType
+    DocumentCard,
+    DocumentCardTitle,
+    DocumentCardActivity,
+    DocumentCardPreview,
+    DocumentCardActions,
+    IDocumentCardPreviewProps,
+    DocumentCardType
 } from 'office-ui-fabric-react/lib/DocumentCard';
 import { ImageFit } from 'office-ui-fabric-react/lib/Image';
 import { Modal } from 'office-ui-fabric-react/lib/Modal';
 import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
+
 // Custom components and properties
 import { IDashdemoProps } from './IDashdemoProps';
 
 // local state
 export interface localState {
-	rowClasses: string;
+    rowClasses: string;
 }
 
 // component class definition
 export default class LinkListContainer extends React.Component<any, localState> {
 
-	// constructor
-	public constructor(props: IDashdemoProps, state: localState) {
-		super(props);
-		//	this.state = {
-		//		rowClasses: "ms-Grid-col ms-sm12"
-		//	};
-	}
+    // constructor
+    public constructor(props: IDashdemoProps, state: localState) {
+        super(props);
+        //	this.state = {
+        //		rowClasses: "ms-Grid-col ms-sm12"
+        //	};
+    }
 
-	public getImageSrc(author) {
-		const filename = author.replace(/\s+/g, '') + ".jpg";
-		const fullUrl = window.location.origin + "/sites/dev/SiteAssets/" + filename;
-		return fullUrl
-	}
+    public getImageSrc(author) {
+        const filename = author.replace(/\s+/g, '') + ".jpg";
+        const fullUrl = window.location.origin + "/sites/dev/SiteAssets/" + filename;
+        return fullUrl
+    }
 
-	// return loading if the incidents state has not yet been set
-	public render(): React.ReactElement<IDashdemoProps> {
-		console.log('linkCard - render - this.props', this.props)
-		const links = this.props.links.slice(0, this.props.showRecentIncidents);
-		const handler = this.props.handler;
-		const rootUrl = window.location.origin;
-		const siteName = "dev"
-		const listName = "SiteAssets"
-		const fileName = "preview200.jpg"
-		let previewURL = rootUrl + "/sites/" + siteName + "/" + listName + "/" + fileName;
+    // return loading if the incidents state has not yet been set
+    public render(): React.ReactElement<IDashdemoProps> {
+        console.log('linkCard - render - this.props', this.props)
+        const links = this.props.links.slice(0, this.props.showRecentIncidents);
+        const handler = this.props.handler;
+        const rootUrl = window.location.origin;
+        const siteName = "dev"
+        const listName = "SiteAssets"
+        const fileName = "preview200.jpg"
+        let previewURL = rootUrl + "/sites/" + siteName + "/" + listName + "/" + fileName;
 
-		if (!links) {
-			return <div>Loading...</div>;
-		}
-		// return list of incidents
-		return (
-			//<div className={styles.panelStyle} >
-			//	<div className={styles.tableStyle} >
-			<div className="ms-Grid">
-				<div className="ms-Grid-row">
-					<div className="ms-Grid-col ms-sm12">
-						{links.map((link, key) => {
+        if (!links) {
+            return <div>Loading...</div>;
+        }
+        // return list of incidents
+        return (
+            //<div className={styles.panelStyle} >
+            //	<div className={styles.tableStyle} >
+            <div className="ms-Grid">
+                <div className="ms-Grid-row">
+                    <div className="ms-Grid-col ms-sm12">
+                        {links.map((link, key) => {
+                            if (link != null) {
+                                const date = new Date(link.Created);
+                                var formatOptions = {
+                                    day: '2-digit',
+                                    month: '2-digit',
+                                    year: 'numeric',
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                    hour12: true
+                                };
+                                const displayDate = date.toLocaleDateString('en-US', formatOptions);
 
-							const date = new Date(link.Created);
-							var formatOptions = {
-								day: '2-digit',
-								month: '2-digit',
-								year: 'numeric',
-								hour: '2-digit',
-								minute: '2-digit',
-								hour12: true
-							};
-							const displayDate = date.toLocaleDateString('en-US', formatOptions);
+                                if (link.incidentPhotos != null) {
+                                    previewURL = link.incidentPhotos.Url;
+                                }
+                                const thisPreviewProps: IDocumentCardPreviewProps = {
+                                    previewImages: [
+                                        {
+                                            previewImageSrc: previewURL,
+                                            imageFit: ImageFit.none
+                                        }
+                                    ],
+                                };
+                                // <DocumentCardPreview { ...thisPreviewProps } />
+                                var fileExt = null;
+                                var fileExtClass = null;
+                                var fileExtClassSm = null;
+                                console.log(link)
+                                if (link.AttachmentFiles.results.length > 0) {
+                                    fileExt = link.AttachmentFiles.results[0].FileName.split(".")[1].trim();
+                                    if (fileExt == 'xlsx' || fileExt == 'xls') {
+                                    // fileExtClass = 'ms-BrandIcon--icon48 ms-BrandIcon--excel'
+                                    // fileExtClassSm = 'ms-BrandIcon--icon16 ms-BrandIcon--excel'
+                                        fileExtClass = 'ms-Icon ms-Icon--ExcelLogo ms-fontSize-su ms-fontColor-green'
+                                        fileExtClassSm = 'ms-Icon ms-Icon--ExcelLogo ms-fontSize-1 ms-fontColor-green'
+                                    } else if (fileExt == 'docx' || fileExt == 'doc') {
+                                        //fileExtClass = 'ms-BrandIcon--icon48 ms-BrandIcon--word'
+                                        //fileExtClassSm = 'ms-BrandIcon--icon16 ms-BrandIcon--word'
+                                        fileExtClass = 'ms-Icon ms-Icon--WordLogo ms-fontSize-su ms-fontColor-blue'
+                                        fileExtClassSm = 'ms-Icon ms-Icon--WordLogo ms-fontSize-1 ms-fontColor-blue'
+                                    } else if (fileExt == 'pptx' || fileExt == 'ppt') {
+                                    //  fileExtClass = 'ms-BrandIcon--icon48 ms-BrandIcon--powerpoint'
+                                    //  fileExtClassSm = 'ms-BrandIcon--icon16 ms-BrandIcon--powerpoint'
+                                        fileExtClass = 'ms-Icon ms-Icon--PowerPointLogo ms-fontSize-su ms-fontColor-redDark'
+                                        fileExtClassSm = 'ms-Icon ms-Icon--PowerPointLogo ms-fontSize-1 ms-fontColor-redDark'
+                                    } else if (fileExt == 'pdf') {
+                                        fileExtClass = 'ms-Icon ms-Icon--PDF ms-fontSize-su ms-fontColor-red'
+                                        fileExtClassSm = 'ms-Icon ms-Icon--PDF ms-fontSize-l ms-fontColor-red'
+                                    } else {
+                                        fileExtClass = 'ms-Icon ms-Icon--Page ms-fontSize-su ms-fontColor-blue'
+                                        fileExtClassSm = 'ms-Icon ms-Icon--Page ms-fontSize-l ms-fontColor-blue'
+                                    }
+                                } else if (link.linkURL != '') {
+                                    fileExtClass = "ms-Icon ms-Icon--Website ms-fontSize-su ms-fontColor-blue"
+                                    fileExtClassSm = "ms-Icon ms-Icon--Website ms-fontSize-l ms-fontColor-blue"
+                                } else {
+                                    fileExtClass = ""
+                                    fileExtClassSm = ""
+                                }
+                                console.log(fileExtClass)
+                                if (this.props.cardType == 0) {
+                                    return (
+                                        <DocumentCard
+                                            className={styles.documentCard}
+                                            onClick={handler.bind(this, link)}>
+                                            <div className={styles.iconContainer}>
+                                                <div className={fileExtClass}></div>
+                                            </div>
+                                            <DocumentCardTitle
+                                                title={link.Title}
+                                                shouldTruncate={true}
+                                            />
+                                            <DocumentCardActivity
+                                                activity={displayDate}
+                                                people={[{
+                                                    name: link.documentAuthor,
+                                                    profileImageSrc: this.getImageSrc(link.documentAuthor)
+                                                }]}
+                                            />
+                                            <DocumentCardActions
+                                                actions={
+                                                    [
+                                                        {
+                                                            iconProps: { iconName: 'Share' },
+                                                            onClick: (ev: any) => {
+                                                                console.log('You clicked the share action.');
+                                                                ev.preventDefault();
+                                                                ev.stopPropagation();
+                                                            },
+                                                            ariaLabel: 'share action'
+                                                        },
+                                                        {
+                                                            iconProps: { iconName: 'Pin' },
+                                                            onClick: (ev: any) => {
+                                                                console.log('You clicked the pin action.');
+                                                                ev.preventDefault();
+                                                                ev.stopPropagation();
+                                                            },
+                                                            ariaLabel: 'pin action'
+                                                        },
+                                                        {
+                                                            iconProps: { iconName: 'Ringer' },
+                                                            onClick: (ev: any) => {
+                                                                console.log('You clicked the ringer action.');
+                                                                ev.preventDefault();
+                                                                ev.stopPropagation();
+                                                            },
+                                                            ariaLabel: 'ringer action'
+                                                        },
+                                                    ]
+                                                }
+                                                views={432}
+                                            />
+                                        </DocumentCard>
+                                    );
+                                } else {
 
-							if (link.incidentPhotos != null) {
-								previewURL = link.incidentPhotos.Url;
-							}
-							const thisPreviewProps: IDocumentCardPreviewProps = {
-								previewImages: [
-									{
-										previewImageSrc: previewURL,
-										imageFit: ImageFit.none
-									}
-								],
-							};
-							if (this.props.cardType == 0) {
-								return (
-									<DocumentCard
-										className={styles.documentCard}
-										onClick={handler.bind(this, link)}>
-										<DocumentCardPreview { ...thisPreviewProps } />
-										<DocumentCardTitle
-											title={link.Title}
-											shouldTruncate={true}
-										/>
-										<DocumentCardActivity
-											activity={displayDate}
-											people={[{
-												name: link.documentAuthor,
-												profileImageSrc: this.getImageSrc(link.documentAuthor)
-											}]}
-										/>
-										<DocumentCardActions
-											actions={
-												[
-													{
-														iconProps: { iconName: 'Share' },
-														onClick: (ev: any) => {
-															console.log('You clicked the share action.');
-															ev.preventDefault();
-															ev.stopPropagation();
-														},
-														ariaLabel: 'share action'
-													},
-													{
-														iconProps: { iconName: 'Pin' },
-														onClick: (ev: any) => {
-															console.log('You clicked the pin action.');
-															ev.preventDefault();
-															ev.stopPropagation();
-														},
-														ariaLabel: 'pin action'
-													},
-													{
-														iconProps: { iconName: 'Ringer' },
-														onClick: (ev: any) => {
-															console.log('You clicked the ringer action.');
-															ev.preventDefault();
-															ev.stopPropagation();
-														},
-														ariaLabel: 'ringer action'
-													},
-												]
-											}
-											views={432}
-										/>
-									</DocumentCard>
-								);
-							} else {
-								return (
-									<DocumentCard
-										type={DocumentCardType.compact}
-										className={styles.documentCard}
-										onClick={handler.bind(this, link)}>
-										<div className='ms-DocumentCard-details'>
-											<DocumentCardTitle
-												title={link.Title}
-												shouldTruncate={true}
-											/>
-											<DocumentCardActivity
-												activity={displayDate}
-												people={[{
-													name: link.documentAuthor,
-													profileImageSrc: this.getImageSrc(link.documentAuthor)
-												}]}
-											/>
-										</div>
-									</DocumentCard>
-								);
-							}
-						})}
-					</div>
-				</div>
-			</div>
-			//	</div>
-			//	</div >
-		);
-	}
+                                    console.log(this.props);
+                                    let previewProps: IDocumentCardPreviewProps = {
+                                    previewImages: [
+                                        {
+                                        name: 'Revenue stream proposal fiscal year 2016 version02.pptx',
+                                        url: 'http://bing.com',
+                                        //previewImageSrc: TestImages.documentPreview,
+                                        iconSrc: 'https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/icon-ppt.png',
+                                        width: 24
+                                        }
+                                    ]
+                                    }
+
+                                    return (
+
+                                    <DocumentCard
+                                        type={DocumentCardType.compact}
+                                        className={styles.documentCard}
+                                        onClick={handler.bind(this, link)}>
+                                        
+                                        <div className='ms-DocumentCard-details'>
+                                        <div className={styles.iconContainer}>
+                                            
+                                        <div className={[fileExtClassSm, styles.inline].join(' ')}></div>
+                                            <div className={[styles.smallTitle,styles.inline].join(' ')}>{link.Title}</div>
+                                        </div>
+                                        <DocumentCardActivity
+                                            activity={displayDate}
+                                            people={[{
+                                                name: link.documentAuthor,
+                                                profileImageSrc: this.getImageSrc(link.documentAuthor)
+                                            }]}
+                                        />
+                                        </div>
+                                    </DocumentCard>
+                                    );
+                                }
+                            } else {
+                                console.log("null element")
+                            }
+                        })}
+                    </div>
+                </div>
+            </div>
+            //	</div>
+            //	</div >
+        );
+    }
 }
